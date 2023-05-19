1e8a7a145e22b1427226e95066972b2e72f27ed1
Lirys <iamyourmarkieboo@gmail.com>
2023-05-19 13:24:36 +0800

    Made dashboard runnable

--- src/Components/Dashboard.java
+++ src/Components/Dashboard.java
@@ -5,11 +5,7 @@ import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.*;
 import java.awt.*;
 
-public class Dashboard extends JFrame {
-
-    public Dashboard(){
-        init();
-    }
+public class Dashboard extends JFrame implements Runnable {
 
     private void init(){
         FlatLightLaf.setup();
@@ -17,6 +13,11 @@ public class Dashboard extends JFrame {
         this.setTitle("Product Manager");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(new Dimension(500,500));
+        this.setVisible(true);
     }
 
+    @Override
+    public void run() {
+        init();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import Components.Dashboard;
 import Components.Themes.ThemeLoader;
 
 public class Main {
 
     public static void main(String[] args) {
         ThemeLoader.loadThemes();
+        Dashboard dash = new Dashboard();
+        dash.run();
     }
 
 }
