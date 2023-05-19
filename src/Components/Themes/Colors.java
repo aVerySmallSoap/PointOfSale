f50f1965f947115430385b408b30106c7dc4a84e
Lirys <iamyourmarkieboo@gmail.com>
2023-05-19 13:00:15 +0800

    Added the utility class Colors

---
+++ src/Components/Themes/Colors.java
@@ -0,0 +1,14 @@
+package Components.Themes;
+
+import java.awt.*;
+
+public class Colors {
+
+    /**
+     * Utility class for color properties to customize different <code>Swing</code> components.
+     */
+
+    public static final Color PRIMARY = new Color(0,0,0,0);
+    public static final Color SECONDARY = new Color(0,0,0,0);
+    public static final Color ACCENT = new Color(0,0,0,0);
+}
