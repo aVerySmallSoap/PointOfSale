d1521b72da58cbdc3a6c385164fb35b0265bd5b1
Lirys <iamyourmarkieboo@gmail.com>
2023-05-19 22:16:23 +0800

    Added themes

---
+++ src/Components/Themes/Classes/SplitTheme.java
@@ -0,0 +1,10 @@
+package Components.Themes.Classes;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+public class SplitTheme extends FlatLightLaf {
+    public static boolean setUp(){
+        return setup(new SplitTheme());
+    }
+
+}
